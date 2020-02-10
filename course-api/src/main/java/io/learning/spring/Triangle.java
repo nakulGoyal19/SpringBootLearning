267f020cbd234ea936effef4c1d032cff30a88bd
nakulGoyal19 <nakul19goyal@gmail.com>
2020-02-11 02:01:05 +0530

    Spring BeanFactory and ApplicationContext(DependencyInjection)

---
+++ course-api/src/main/java/io/learning/spring/Triangle.java
@@ -0,0 +1,30 @@
+package io.learning.spring;
+
+public class Triangle {
+	private String type;
+	private int height;	
+	public Triangle(String type) {
+		this.type = type;
+	}
+
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void draw() {
+		System.out.println(getType()+" Drawn Triangle of height : "+getHeight());
+	}
+}
