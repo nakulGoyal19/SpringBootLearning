a91cca72ba6d279c3590a15620b5ba0cf3fc01b4
nakulGoyal19 <nakul19goyal@gmail.com>
2020-03-20 15:48:30 +0530

    Unit test cases for BankController and updated exception handling.

--- BankManagementApplication/src/main/java/com/BankManagementApplication/Controllers/BankController.java
+++ BankManagementApplication/src/main/java/com/BankManagementApplication/Controllers/BankController.java
@@ -1,11 +1,14 @@
 package com.BankManagementApplication.Controllers;
 
+import com.BankManagementApplication.Exception.DataNotFoundException;
 import com.BankManagementApplication.Models.UserProfile;
 import com.BankManagementApplication.Services.OperationService;
 import com.BankManagementApplication.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.InvalidNameException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -19,27 +22,58 @@ public class BankController {
     @Autowired
     OperationService operationService;
 
+    public BankController(UserService userService, OperationService operationService) {
+        this.userService = userService;
+        this.operationService = operationService;
+    }
+
     @GetMapping("/allUsersData")
     public List<UserProfile> getAllUserData(){
 
-        return userService.getAllUsersData();
+        List<UserProfile> userProfileList = userService.getAllUsersData();
+        if(userProfileList == null)
+            throw new NullPointerException("Null returned by UserService");
+        if(userProfileList.size() == 0)
+            throw new DataNotFoundException("No data available.");
+        return userProfileList;
     }
 
     @PostMapping("/addUser")
-    public void addUser(@RequestBody UserProfile userProfile){
-        userService.addUser(userProfile);
-
-        operationService.addOperationAddUser(Calendar.getInstance().getTime(),"new user added",userProfile);
+    public String addUser(@RequestBody UserProfile userProfile) throws InvalidNameException {
+        String str = userProfile.getFirstName().concat(userProfile.getLastName());
+        if((str != null)
+                && (!str.equals(""))
+                && (str.matches("^[a-zA-Z]*$"))) {
+            userService.addUser(userProfile);
+            operationService.addOperationAddUser(Calendar.getInstance().getTime(), "new user added", userProfile);
+            return "User Added Successfully";
+        }
+        else throw new InvalidNameException("Name contains invalid data");
     }
 
     @GetMapping("/findByAccountNumber")
     public UserProfile getUserByAccountNumber(@RequestParam String accountNumber){
-        return userService.getUserByAccountNumber(accountNumber);
+        UserProfile userProfile = userService.getUserByAccountNumber(accountNumber);
+        if(userProfile == null)
+            throw new DataNotFoundException("No account with this account number");
+        return userProfile;
     }
 
     @GetMapping("/findByFirstName")
-    public List<UserProfile> getUserByFirstName(@RequestParam String firstName){
-        return userService.getUserByFirstName(firstName);
+    public List<UserProfile> getUserByFirstName(@RequestParam String firstName) throws InvalidNameException {
+        List<UserProfile> userProfileList;
+        if((firstName != null)
+                && (!firstName.equals(""))
+                && (firstName.matches("^[a-zA-Z]*$"))) {
+            userProfileList = userService.getUserByFirstName(firstName);
+            if(userProfileList == null)
+                throw new NullPointerException("Null returned by the service.");
+            if(userProfileList.size()==0)
+                throw new DataNotFoundException("No users with this first name.");
+        }
+        else
+            throw new InvalidNameException("First Name is not valid");
+        return userProfileList;
     }
 
     @PutMapping("/credit")
---
+++ BankManagementApplication/src/test/java/com/BankManagementApplication/BankControllerTesting.java
@@ -0,0 +1,85 @@
+package com.BankManagementApplication;
+
+import com.BankManagementApplication.Controllers.BankController;
+import com.BankManagementApplication.Exception.DataNotFoundException;
+import com.BankManagementApplication.Models.UserProfile;
+import com.BankManagementApplication.Services.OperationService;
+import com.BankManagementApplication.Services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.naming.InvalidNameException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankControllerTesting {
+
+
+    @Autowired
+    BankController bankController;
+
+    OperationService operationService = Mockito.mock(OperationService.class);
+    UserService userService = Mockito.mock(UserService.class);
+
+    @BeforeEach
+    public void setUp(){
+        bankController = new BankController(userService,operationService);
+    }
+
+    @Test
+    public  void getAllUserDataTesting(){
+        //get correct data
+        Mockito.when(userService.getAllUsersData()).thenReturn(new ArrayList<>( Arrays.asList(new UserProfile("00001", "Nakul", "Goyal", 2000),
+                new UserProfile("00002", "Rahul", "Goyal", 2000),
+                new UserProfile("00003", "Anant", "Shibe", 2000))));
+
+        assertTrue(bankController.getAllUserData()!=null);
+
+        //get empty list
+        Mockito.when(userService.getAllUsersData()).thenReturn(new ArrayList<>( Arrays.asList()));
+        assertThrows(DataNotFoundException.class,()->bankController.getAllUserData());
+
+        //get null
+        Mockito.when(userService.getAllUsersData()).thenReturn(null);
+        assertThrows(NullPointerException.class,()->bankController.getAllUserData());
+
+    }
+
+    @Test
+    public void addUserTesting() throws InvalidNameException {
+
+        UserProfile userProfile = new UserProfile("00001","Nakul","0Goyal",90);
+        assertThrows(InvalidNameException.class,()->bankController.addUser(userProfile));
+
+        UserProfile userProfile1 = new UserProfile("00001","Nakul","Goyal",90);
+        assertEquals("User Added Successfully",bankController.addUser(userProfile1));
+
+    }
+
+    @Test
+    public void getUserByAccountNumberTesting(){
+        Mockito.when(userService.getUserByAccountNumber("00001")).thenReturn(null);
+        assertThrows(DataNotFoundException.class,()->bankController.getUserByAccountNumber("00001"));
+    }
+
+    @Test
+    public void getUserByFirstNameTesting(){
+        //Empty list
+        Mockito.when(userService.getUserByFirstName("Nakul")).thenReturn(new ArrayList<>(Arrays.asList()));
+        assertThrows(DataNotFoundException.class,()->bankController.getUserByFirstName("Nakul"));
+
+        //Null
+        Mockito.when(userService.getUserByFirstName("Nakul")).thenReturn(null);
+        assertThrows(NullPointerException.class,()->bankController.getUserByFirstName("Nakul"));
+
+        //Invalid name
+        assertThrows(InvalidNameException.class,()->bankController.getUserByFirstName("Nakul0"));
+    }
+
+    
+
+}
