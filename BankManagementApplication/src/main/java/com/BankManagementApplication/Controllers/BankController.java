084e7770e903f13b644ac602a781b6b5ca50f8da
nakulGoyal19 <nakul19goyal@gmail.com>
2020-03-30 23:23:30 +0530

    added KafkaTemplate messaging to BankController

--- BankManagementApplication/src/main/java/com/BankManagementApplication/Controllers/BankController.java
+++ BankManagementApplication/src/main/java/com/BankManagementApplication/Controllers/BankController.java
@@ -5,10 +5,9 @@ import com.BankManagementApplication.Models.UserProfile;
 import com.BankManagementApplication.Services.OperationService;
 import com.BankManagementApplication.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
-
 import javax.naming.InvalidNameException;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -18,7 +17,11 @@ public class BankController {
 
     @Autowired
     UserService userService;
-
+    
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+    private static final String TOPIC = "bankManagement";
+    
     @Autowired
     OperationService operationService;
 
@@ -29,23 +32,26 @@ public class BankController {
 
     @GetMapping("/allUsersData")
     public List<UserProfile> getAllUserData(){
-
+        kafkaTemplate.send(TOPIC,"Getting all users data.");
         List<UserProfile> userProfileList = userService.getAllUsersData();
         if(userProfileList == null)
             throw new NullPointerException("Null returned by UserService");
         if(userProfileList.size() == 0)
             throw new DataNotFoundException("No data available.");
+        kafkaTemplate.send(TOPIC,"All user Data fetched successfully");
         return userProfileList;
     }
 
     @PostMapping("/addUser")
     public String addUser(@RequestBody UserProfile userProfile) throws InvalidNameException {
+        kafkaTemplate.send(TOPIC,"Adding new user initialised");
         String str = userProfile.getFirstName().concat(userProfile.getLastName());
         if((str != null)
                 && (!str.equals(""))
                 && (str.matches("^[a-zA-Z]*$"))) {
             userService.addUser(userProfile);
             operationService.addOperationAddUser(Calendar.getInstance().getTime(), "new user added", userProfile);
+            kafkaTemplate.send(TOPIC,"User added successfully");
             return "User Added Successfully";
         }
         else throw new InvalidNameException("Name contains invalid data");
@@ -53,14 +59,17 @@ public class BankController {
 
     @GetMapping("/findByAccountNumber")
     public UserProfile getUserByAccountNumber(@RequestParam String accountNumber){
+        kafkaTemplate.send(TOPIC,"Finding user by account number initialised");
         UserProfile userProfile = userService.getUserByAccountNumber(accountNumber);
         if(userProfile == null)
             throw new DataNotFoundException("No account with this account number");
+        kafkaTemplate.send(TOPIC,"Finding user by account number completed");
         return userProfile;
     }
 
     @GetMapping("/findByFirstName")
     public List<UserProfile> getUserByFirstName(@RequestParam String firstName) throws InvalidNameException {
+        kafkaTemplate.send(TOPIC,"Finding users by first name initialised");
         List<UserProfile> userProfileList;
         if((firstName != null)
                 && (!firstName.equals(""))
@@ -73,22 +82,29 @@ public class BankController {
         }
         else
             throw new InvalidNameException("First Name is not valid");
+        kafkaTemplate.send(TOPIC,"Finding users by first name completed");
         return userProfileList;
     }
 
     @PutMapping("/credit")
     public String credit(@RequestParam String accountNumber,@RequestParam Integer creditAmount){
+        kafkaTemplate.send(TOPIC,"Amount credit initialised");
+    
         String res = userService.credit(accountNumber,creditAmount);
         if(res == "credit successful")
             operationService.addOperation(Calendar.getInstance().getTime(),"amount credited",accountNumber);
+        kafkaTemplate.send(TOPIC,res);
         return res;
     }
 
     @PutMapping("/debit")
     public String debit(@RequestParam String accountNumber,@RequestParam Integer debitAmount){
+        kafkaTemplate.send(TOPIC,"Amount debit initialised");
+    
         String res = userService.debit(accountNumber,debitAmount);
         if(res != "Insufficient balance")
             operationService.addOperation(Calendar.getInstance().getTime(),"amount debited",accountNumber);
+        kafkaTemplate.send(TOPIC,res);
         return res;
     }
 
