53125fc1a696552b5e6aaa196fb41a08e4545c20
nakulGoyal19 <nakul19goyal@gmail.com>
2020-04-01 15:16:17 +0530

    Kafka Consumer setup

--- BankManagementApplication/src/main/java/com/BankManagementApplication/Listner/KafkaConsumer.java
+++ BankManagementApplication/src/main/java/com/BankManagementApplication/Listner/KafkaConsumer.java
@@ -1,11 +1,15 @@
 package com.BankManagementApplication.Listner;
 
-/**
- * @author nakulgoyal
- *         01/04/20
- **/
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaConsumer {
     
+    @KafkaListener(topics = "bankManagement", groupId = "bank")
+    public  void consume(String message){
+        System.out.println("Consumed message: "+message);
+    }
 }
 
 
